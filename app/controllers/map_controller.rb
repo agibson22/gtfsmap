c966c753a54a21de9e43829d165c68eb46882597
Alex Gibson <agibson22@gmail.com>
2017-03-28 19:59:37 -0400

    getting closer...

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,9 @@
 class MapController < ApplicationController
   def index
+ 	# @trip_id = entity.vehicle.trip.trip_id
+	# @vehicle_id = entity.vehicle.vehicle.id
+	# @latitude = entity.vehicle.position.latitude
+	# @longitude = entity.vehicle.position.longitude
+	# @heading = entity.vehicle.position.bearing
   end
 end
--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,2 +1,17 @@
 module MapHelper
+	def get_gtfs_rt
+
+		gtfs_rt_url = "http://api.octa.net/GTFSRealTime/protoBuf/VehiclePositions.aspx"
+		data = Net::HTTP.get(URI.parse(gtfs_rt_url))
+		feed = Transit_realtime::FeedMessage.decode(data)
+		for entity in feed.entity do
+			if entity.field?(:vehicle)
+				@trip_id = entity.vehicle.trip.trip_id
+				@vehicle_id = entity.vehicle.vehicle.id
+				@latitude = entity.vehicle.position.latitude
+				@longitude = entity.vehicle.position.longitude
+				@heading = entity.vehicle.position.bearing
+			end
+		end
+	end
 end
