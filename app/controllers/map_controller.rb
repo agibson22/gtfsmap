79e7ad74fc9934f869e74e050e9d9434f6b06ee6
Alex Gibson <agibson22@gmail.com>
2017-03-30 20:08:27 -0400

    gtfs rt vehicles on static map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,9 +1,24 @@
+require 'net/http'
+
 class MapController < ApplicationController
+  include Transit_realtime
   def index
- 	# @trip_id = entity.vehicle.trip.trip_id
-	# @vehicle_id = entity.vehicle.vehicle.id
-	# @latitude = entity.vehicle.position.latitude
-	# @longitude = entity.vehicle.position.longitude
-	# @heading = entity.vehicle.position.bearing
+ 	gtfs_rt_url = "http://api.octa.net/GTFSRealTime/protoBuf/VehiclePositions.aspx"
+
+	data = Net::HTTP.get(URI.parse(gtfs_rt_url))
+	feed = Transit_realtime::FeedMessage.decode(data)
+	vehicle_hashes = []
+	feed.entity[0..10].each do |v|
+		if v.field?(:vehicle)
+			vehicle_info = { 
+				"id" => v.vehicle.vehicle.id,
+				"lat" => v.vehicle.position.latitude,
+				"long" => v.vehicle.position.longitude
+				}
+			vehicle_hashes << vehicle_info
+		end
+	end
+	
+	@vehicle_hashes = vehicle_hashes
   end
 end
--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -2,16 +2,20 @@ module MapHelper
 	def get_gtfs_rt
 
 		gtfs_rt_url = "http://api.octa.net/GTFSRealTime/protoBuf/VehiclePositions.aspx"
+
 		data = Net::HTTP.get(URI.parse(gtfs_rt_url))
 		feed = Transit_realtime::FeedMessage.decode(data)
-		for entity in feed.entity do
-			if entity.field?(:vehicle)
-				@trip_id = entity.vehicle.trip.trip_id
-				@vehicle_id = entity.vehicle.vehicle.id
-				@latitude = entity.vehicle.position.latitude
-				@longitude = entity.vehicle.position.longitude
-				@heading = entity.vehicle.position.bearing
+		vehicle_hashes = []
+		feed.entity[0..10].each do |v|
+			if v.field?(:vehicle)
+				vehicle_info = { 
+					"id" => v.vehicle.vehicle.id,
+					"lat" => v.vehicle.position.latitude,
+					"long" => v.vehicle.position.longitude
+					}
+				vehicle_hashes << vehicle_info
 			end
 		end
+
 	end
 end
